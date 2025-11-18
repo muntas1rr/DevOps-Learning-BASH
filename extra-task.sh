1d8f87d6134a9b7e2f9c16f8aa472cd737203cca
muntas1rr <muntasir.t.hoque@gmail.com>
2025-11-17 23:51:25 -0600

    Add extra-task.sh

---
+++ extra-task.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+: '
+Extra Creative Task:
+Challenge: Create a simple shell script that automates a common task on your system.
+This script checks the Desktop folder: counts files, shows size, lists files, and prints text content.
+'
+
+echo ""
+echo "===== DESKTOP REPORT ====="
+
+desktop_count=$(ls -1A ~/Desktop | wc -l)
+desktop_size=$(du -sh ~/Desktop | cut -f1)
+
+echo "Total items: $desktop_count"
+echo "Total size: $desktop_size"
+
+# List files if there are any
+if [[ $desktop_count -gt 0 ]]; then
+    echo ""
+    echo "Files on Desktop:"
+    ls -l ~/Desktop
+    echo ""
+fi
